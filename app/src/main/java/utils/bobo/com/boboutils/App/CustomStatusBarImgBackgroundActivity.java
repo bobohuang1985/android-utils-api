4b5113f5f68cde441b7ab138229c9a0979760278
huangzb1 <huangzb1@lenovo.com>
2019-03-05 23:32:29 +0800

    添加沉浸式状态栏（使用图片背景）demo

---
+++ app/src/main/java/utils/bobo/com/boboutils/App/CustomStatusBarImgBackgroundActivity.java
@@ -0,0 +1,31 @@
+package utils.bobo.com.boboutils.App;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import utils.bobo.com.boboutils.R;
+
+public class CustomStatusBarImgBackgroundActivity extends Activity {
+    private static final String TAG = "CustomStatusBarImgBackgroundActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21){
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        setContentView(R.layout.activity_custom_statusbar_img_bg);
+    }
+}
